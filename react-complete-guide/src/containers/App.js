2195c48210917c7ae72c0b1b4368590562d0be2a
Nathália Pissuti <nathalia.pissuti@venturus.org.br>
2020-02-04 12:50:16 -0300

    Mudando estrutura do projeto

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-
-import Person from './Person/Person';
+import Persons from '../components/Persons/Persons';
 
 import classes from './App.css';
 
@@ -47,15 +46,11 @@ class App extends Component {
         if(this.state.showPersons) {
             persons = (
                 <div>
-                    {this.state.persons.map((person, index) => {
-                        return <Person
-                            key={person.id}
-                            click={() => this.deletePersonHandler(index)}
-                            name={person.name} 
-                            age={person.age} 
-                            changed={(event) => this.nameChangeHandler(event, person.id)}
-                        />
-                    })}
+                    <Persons
+                        persons={this.state.persons} 
+                        clicked={this.deletePersonHandler} 
+                        changed={this.nameChangeHandler}
+                    />
                 </div>
             );
 
