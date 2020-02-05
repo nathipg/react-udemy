49c4a9b0eaa54ed3cdc689d77b228dc0986a26d1
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-02-05 17:36:47 -0300

    Usando Fragment

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-
-import { Aux } from '../../../hoc/Aux';
+import React, { Component, Fragment } from 'react';
 
 import classes from './Person.css';
 
@@ -8,13 +6,13 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <Aux>
-                <div className={classes.Person}>
+            <Fragment>
+                {/* <div className={classes.Person}> */}
                     <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>
                     <p>{this.props.children}</p>
                     <input type="text" onChange={this.props.changed} value={this.props.name} />
-                </div>
-            </Aux>
+                {/* </div> */}
+            </Fragment>
         );
     }
 }
