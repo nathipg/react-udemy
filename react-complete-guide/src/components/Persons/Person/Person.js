2d5ef36201e5b29df3c7a228d4bbda7759e23010
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-02-05 18:12:16 -0300

    Atualizando o estado dependendo valor anterior

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -6,13 +6,11 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <Fragment>
-                {/* <div className={classes.Person}> */}
-                    <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>
-                    <p>{this.props.children}</p>
-                    <input type="text" onChange={this.props.changed} value={this.props.name} />
-                {/* </div> */}
-            </Fragment>
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed} value={this.props.name} />
+            </div>
         );
     }
 }
