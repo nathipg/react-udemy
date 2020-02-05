6b3b0c929cdfe94d9c328d73752b6d9098a9e796
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-02-05 18:18:56 -0300

    PropTypes

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Person.css';
 
@@ -14,5 +15,12 @@ class Person extends Component {
         );
     }
 }
+
+Person.propTypes = {
+    age: PropTypes.number,
+    click: PropTypes.func,
+    changed: PropTypes.func,
+    name: PropTypes.string
+};
 
 export default Person;
