05c96c6f9d7d415bc5c2b381a03be90a36434841
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-02-06 12:08:28 -0300

    Referencias com hooks

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,10 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import classes from './Cockpit.css';
 
 const Cockpit = props => {
+    const toggleBtnRef = useRef(null);
+
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
+
+        toggleBtnRef.current.click();
 
         return () => {
             console.log('[Cockpit.js] Cleanup work in useEffect');
@@ -37,7 +41,7 @@ const Cockpit = props => {
         <div className={classes.Cockpit}>
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>That's it</p>
-            <button className={btnClass} onClick={props.clicked}>
+            <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
                 Toggle Person
             </button>
         </div>
