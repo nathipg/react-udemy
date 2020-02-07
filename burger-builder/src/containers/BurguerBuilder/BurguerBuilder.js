b3a4457d97a6e885d7b339ba2d64d580cb3d6445
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-02-07 10:29:31 -0300

    BurguerBuilder container

---
+++ burger-builder/src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from 'react';
+
+class BurguerBuilder extends Component {
+    render() {
+        return (
+            <Fragment>
+               <div>Burguer</div>
+               <div>Build Controls</div>
+            </Fragment>
+        );
+    }
+}
+
+export default BurguerBuilder;
