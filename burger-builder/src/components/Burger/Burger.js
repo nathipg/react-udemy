ce16725fcdc2f25d63df063d20fa2d89636a5988
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-02-07 13:02:07 -0300

    Tratamento hamburguer vazio

--- burger-builder/src/components/Burger/Burger.js
+++ burger-builder/src/components/Burger/Burger.js
@@ -4,17 +4,24 @@ import BurguerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const Burguer = props => {
-    const ingredientsList = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(key => {
             return [...Array(props.ingredients[key])].map((_, i) => {
                 return <BurguerIngredient key={key + i} type={key} />
             });
-        });
+        })
+        .reduce((list, e) => {
+            return list.concat(e);
+        }, []);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients</p>
+    }
 
     return (
         <div className={classes.Burguer}>
             <BurguerIngredient type="bread-top" />
-            {ingredientsList}
+            {transformedIngredients}
             <BurguerIngredient type="bread-bottom" />
         </div>
     );
