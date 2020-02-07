bc4351d3a931e537007cf0454480d233babd8ea6
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-02-07 10:18:00 -0300

    Layout component

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+
 class App extends Component {
-  render() {
-    return (
-      <div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Layout>
+                <p>Test</p>
+            </Layout>
+        );
+    }
 }
 
 export default App;
