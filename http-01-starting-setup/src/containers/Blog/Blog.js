17c9e9361c3767196457413d66916a37969d60d4
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-02-10 16:43:34 -0300

    NavLink estilizado

--- http-01-starting-setup/src/containers/Blog/Blog.js
+++ http-01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -13,8 +13,8 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to="/new-post">New Post</Link></li>
+                            <li><NavLink to="/" exact activeStyle={{textDecoration: 'underline'}}>Home</NavLink></li>
+                            <li><NavLink to="/new-post">New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
